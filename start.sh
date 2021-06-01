e85bd6985f79b257494895784ec94f3255580703
Alberto <alberto.rinaudo+torann@gmail.com>
2021-06-01 14:59:41 +0100

    init with 6.3
    Signed-off-by: Alberto <alberto.rinaudo+torann@gmail.com>

---
+++ start.sh
@@ -0,0 +1,41 @@
+#/bin/sh
+
+APP_NAME="Communicator"
+
+export QT_PLUGIN_PATH="/app/opt/$APP_NAME/lib/plugins"
+export LD_LIBRARY_PATH="/app/opt/$APP_NAME/lib"
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export QT_XCB_GL_INTEGRATION=none
+export LIBGL_ALWAYS_INDIRECT=1
+
+#check if input arguments contain 'glocom://'
+flag1=`echo $@|awk '{print match($0,"glocom://")}'`;
+flag2=`echo $@|awk '{print match($0,"tel://")}'`;
+flag3=`echo $@|awk '{print match($0,"sip://")}'`;
+flag4=`echo $@|awk '{print match($0,"callto://")}'`;
+
+if [ $flag1 -gt 0 ];then
+        #remove spaces from input arguiments
+        number=`echo "$@" | sed 's/ //g'`
+        /app/opt/$APP_NAME/bin/$APP_NAME $number
+                exit 1
+elif [ $flag2 -gt 0 ];then
+        #remove spaces from input arguiments
+        number=`echo "$@" | sed 's/ //g' | sed 's/tel/glocom/'`
+        /app/opt/$APP_NAME/bin/$APP_NAME $number
+                exit 1
+elif [ $flag3 -gt 0 ];then
+        #remove spaces from input arguiments
+        number=`echo "$@" | sed 's/ //g' | sed 's/sip/glocom/'`
+        /app/opt/$APP_NAME/bin/$APP_NAME $number
+                exit 1
+elif [ $flag4 -gt 0 ];then
+        #remove spaces from input arguiments
+        number=`echo "$@" | sed 's/ //g' | sed 's/callto/glocom/'`
+        /app/opt/$APP_NAME/bin/$APP_NAME $number
+                exit 1
+else
+        /app/opt/$APP_NAME/bin/$APP_NAME $1
+                exit $?
+fi
+
